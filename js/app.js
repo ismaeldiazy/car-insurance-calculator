f3d66cff25f0e7c9acc272d6c0cd3d3985551f59
ismaeldiazy <ismaeldiazy@gmail.com>
2020-02-09 15:23:49 +0100

    Added querySelectors to read the value from the selectors and radio buttons, and conditional statement for checking if the inputs are empty.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 // Insurance constructor
-function Insurance() {
-
+function Insurance(brand, year, type) {
+    this.brand = brand;
+    this.year = year;
+    this.type = type;
 }
+
+// GUI constructor
+function Interface() {}
+
+// EventListeners
+const form = document.getElementById('cotizar-seguro');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Read the selected brand from the selector
+    const brand = document.getElementById('marca');
+    const selectedBrand = brand.options[brand.selectedIndex].value
+
+    // Read the year
+    const year = document.getElementById('anio');
+    const selectedYear = year.options[year.selectedIndex].value
+
+    // Read value from radio button
+    const type = document.querySelector('input[name=tipo]:checked').value;
+
+    // Create interface instance
+    const interface = new Interface()
+
+    // Check that fields are not empty
+    if (selectedBrand === '' || selectedYear === '' || type === '') {
+        console.log('Faltan datos');
+    }else {
+        console.log('Tot be my dude');
+    }
+} )
 
 // Print select options
 const max = new Date().getFullYear(),
